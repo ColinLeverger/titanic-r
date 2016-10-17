fed2930fdd24f66d92bc0e71204dbbce59c9949f
Colin LEVERGER <colinleverger@gmail.com>
2016-10-17 10:47:51 +0200

    Update & refactoring of the code

--- titanic.R
+++ titanic.R
@@ -13,7 +13,7 @@ library(reshape2)
 library(vcd)
 
 # Set working directory
-setwd("/Users/colinleverger/Downloads/titanic-ml/")
+setwd("/Users/colinleverger/Documents/Experiments/kaggle/titanic-ml")
 
 # Load data
 missing.types <- c("NA", "")
@@ -30,66 +30,6 @@ checkDataQuality(
 dqr.cont <- read.csv("DQR_cont.csv")
 dqr.cat  <- read.csv("DQR_cat.csv")
 
-#### Work on data ####
-### Extract Titles and create new categorical column ###
-train.data$Title   <- gsub('(.*, )|(\\..*)', '', train.data$Name)
-train.data$Name    <- gsub('(, [a-zA-Z]{,20}. )', ', ', train.data$Name)
-train.data$Surname <- gsub('(.*,)', '', train.data$Name)
-train.data$Name    <- gsub('(,.*)', '', train.data$Name)
-
-# Rare titles management
-rare.titles <-
-  c(
-    'Dona',
-    'Lady',
-    'the Countess',
-    'Capt',
-    'Col',
-    'Don',
-    'Dr',
-    'Major',
-    'Rev',
-    'Sir',
-    'Jonkheer'
-  )
-
-# Replace rare titles with correct values
-train.data$Title[train.data$Title == 'Mlle']        <- 'Miss'
-train.data$Title[train.data$Title == 'Ms']          <- 'Miss'
-train.data$Title[train.data$Title == 'Mme']         <- 'Mrs'
-train.data$Title[train.data$Title %in% rare.titles]  <- 'RareTitle'
-
-### Create families description ###
-# First, add family size
-train.data$FamilySize = train.data$SibSp + train.data$Parch + 1
-
-# Then, add family size categorical feature
-train.data$FamilySizeD[train.data$FamilySize == 1] <- 
-  'singleton'
-train.data$FamilySizeD[train.data$FamilySize > 1 & train.data$FamilySize < 5] <- 
-  'small'
-train.data$FamilySizeD[train.data$FamilySize >= 5] <- 
-  'big'
-
-# Deal with missing boarding
-View(train.data[is.na(train.data$Embarked),])
-
-# Get rid of our missing passenger IDs
-embark.fare <- train.data %>%
-  filter(PassengerId != 62 & PassengerId != 830)
-
-# Use ggplot2 to visualize embarkment, passenger class, & median fare
-ggplot(embark.fare, aes(x = Embarked, y = Fare, fill = factor(Pclass))) +
-  geom_boxplot() +
-  geom_hline(aes(yintercept=80), 
-             colour='red', linetype='dashed', lwd=2) +
-  scale_y_continuous()
-
-train.data[(train.data$PassengerId == 62 | train.data$PassengerId == 830),]$Embarked <- "C"
-
-# Deal with guys with several cabins (family?)
-train.data$SeveralCabins <- 0
-train.data[grepl(" ", train.data$Cabin),]$SeveralCabins <- 1
 
 #### Explore the data ####
 # Explore difference between men's and women's death
@@ -209,41 +149,8 @@ boxplot(
   ylab = "Age"
 )
 
-# Compute median and mean for age
-mean.age <- train.data[!is.na(train.data$Age),] %>%
-  group_by(Sex) %>%
-  summarise(Mean = mean(Age),
-            Mediane = median(Age))
-
-# Display repartition of the titles
-barplot(
-  table(train.data$Title),
-  main = "Title",
-  col = "blue"
-)
-
 # Link it to the sex...
 table(train.data$Sex, train.data$Title)
-
-# Explore result again
-barplot(
-  table(train.data$Title),
-  main = "Titles",
-  col = "blue"
-)
-table(train.data$Sex, train.data$Title)
-
-# Explore families 
-# Size:
-barplot(table(train.data$FamilySize),
-        main = "Family size repartition",
-        col = "red")
-
-# Survival vs Size:
-ggplot(train.data, aes(x = FamilySize, fill = factor(Survived))) +
-  geom_bar(stat = 'count', position = 'dodge') +
-  scale_x_continuous(breaks = c(1:11)) +
-  labs(x = 'Family Size')
 
 #### Duplicated ticket number investigations ####
 duplicated.tickets <- train.data %>%
@@ -293,11 +200,125 @@ barplot(table(dupes.ticket.survival.rate$SurvivalRateD),
         main = "Survival rate for duplicated tickets",
         col = "violet")
 
-#### Dummy method: replace age by median for gender ####
-train.data[is.na(train.data$Age) &
-             train.data$Sex == "male",]$Age <-
+#### Work on data ####
+### Extract Titles and create new categorical column ###
+total.data$Title   <- gsub('(.*, )|(\\..*)', '', total.data$Name)
+total.data$Name    <- gsub('(, [a-zA-Z]{,20}. )', ', ', total.data$Name)
+total.data$Surname <- gsub('(.*,)', '', total.data$Name)
+total.data$Name    <- gsub('(,.*)', '', total.data$Name)
+
+# Display repartition of the titles
+barplot(
+  table(total.data$Title),
+  main = "Title",
+  col = "blue"
+)
+
+# Rare titles management
+rare.titles <-
+  c(
+    'Dona',
+    'Lady',
+    'the Countess',
+    'Capt',
+    'Col',
+    'Don',
+    'Dr',
+    'Major',
+    'Rev',
+    'Sir',
+    'Jonkheer'
+  )
+
+# Replace rare titles with correct values
+total.data$Title[total.data$Title == 'Mlle']        <- 'Miss'
+total.data$Title[total.data$Title == 'Ms']          <- 'Miss'
+total.data$Title[total.data$Title == 'Mme']         <- 'Mrs'
+total.data$Title[total.data$Title %in% rare.titles]  <- 'RareTitle'
+
+# Display result again
+barplot(
+  table(total.data$Title),
+  main = "Titles",
+  col = "blue"
+)
+table(total.data$Sex, total.data$Title)
+
+### Create families description ###
+# First, add family size
+total.data$FamilySize = total.data$SibSp + total.data$Parch + 1
+
+# Explore family size
+barplot(table(total.data$FamilySize),
+        main = "Family size repartition",
+        col = "red")
+
+# Survival vs Size:
+ggplot(total.data, aes(x = FamilySize, fill = factor(Survived))) +
+  geom_bar(stat = 'count', position = 'dodge') +
+  scale_x_continuous(breaks = c(1:11)) +
+  labs(x = 'Family Size')
+
+# Then, add family size categorical feature
+total.data$FamilySizeD[total.data$FamilySize == 1] <- 
+  'singleton'
+total.data$FamilySizeD[total.data$FamilySize > 1 & total.data$FamilySize < 5] <- 
+  'small'
+total.data$FamilySizeD[total.data$FamilySize >= 5] <- 
+  'big'
+
+### Deal with missing boarding
+View(total.data[is.na(total.data$Embarked),])
+
+# Get rid of our missing passenger IDs
+embark.fare <- total.data %>%
+  filter(PassengerId != 62 & PassengerId != 830)
+
+# Use ggplot2 to visualize embarkment, passenger class, & median fare
+ggplot(embark.fare, aes(x = Embarked, y = Fare, fill = factor(Pclass))) +
+  geom_boxplot() +
+  geom_hline(aes(yintercept=80), 
+             colour='red', linetype='dashed', lwd=2) +
+  scale_y_continuous()
+
+total.data[(total.data$PassengerId == 62 | total.data$PassengerId == 830),]$Embarked <- "C"
+
+### Deal with guys with several cabins (family?)
+total.data$SeveralCabins <- 0
+total.data[grepl(" ", total.data$Cabin),]$SeveralCabins <- 1
+
+### Deal with missing fares
+View(total.data[train.data$Fare <= 10 | is.na(total.data$Fare),] %>% arrange(Fare))
+View(total.data[is.na(total.data$Fare),] %>% arrange(Fare))
+ggplot(total.data[total.data$Pclass == '3' & total.data$Embarked == 'S', ], 
+       aes(x = Fare)) +
+  geom_density(fill = '#99d6ff', alpha=0.4) + 
+  geom_vline(aes(xintercept=median(Fare, na.rm=T)),
+             colour='red', linetype='dashed', lwd=1)
+
+total.data$Fare[1044] <- median(total.data[total.data$Pclass == '3' & total.data$Embarked == 'S', ]$Fare, na.rm = TRUE)
+
+### Add some meaningfull features
+# Women and children before
+total.data$WomOrChildren <- 0
+total.data[which(total.data$Age < 18 | total.data$Sex == 'female'),]$WomOrChildren <- 1
+
+# First char of cabin is the deck
+total.data$Deck <- substring(total.data$Cabin, 1, 1)
+total.data$Deck[which(is.na(total.data$Deck))] <- "NoSe"
+
+### Missing ages management
+# Method 1: mean age method
+mean.age <- total.data[!is.na(total.data$Age),] %>%
+  group_by(Sex) %>%
+  summarise(Mean = mean(Age),
+            Mediane = median(Age))
+
+total.data[is.na(total.data$Age) & total.data$Sex == "male",]$Age <-
   mean.age[mean.age$Sex == 'male',]$Mediane
 
-train.data[is.na(train.data$Age) &
-             train.data$Sex == "female",]$Age <-
+total.data[is.na(total.data$Age) & total.data$Sex == "female",]$Age <-
   mean.age[mean.age$Sex == 'female',]$Mediane
+
+# Method 2: predictive imputation
+# TODO
